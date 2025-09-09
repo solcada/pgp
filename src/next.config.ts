99a9072ec606ab2836b342b04448e6d3eb9a498c
solcada <andrew.dotchin@solcada.com>
2025-09-09 16:34:54 +1200

    adding

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
-const base = process.env.BASE_PATH || '';
-
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: '../out',
-    basePath: base,                 // e.g. "/your-repo"
-    assetPrefix: base ? `${base}/` : '',
-    images: { unoptimized: true },  // needed for static export
-    trailingSlash: true,            // avoids 404 on refresh
 };
 
 export default nextConfig;
