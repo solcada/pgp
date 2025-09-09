adace901ced2e46cda850d1c05f0150d5f8b2201
solcada <andrew.dotchin@solcada.com>
2025-09-09 16:39:22 +1200

    making things localized

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+// src/next.config.js
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: '../out',
+    basePath: repoName ? `/${repoName}` : "",
+    assetPrefix: repoName ? `/${repoName}/` : "",
+    images: { unoptimized: true },
+    trailingSlash: true,    
 };
 
 export default nextConfig;
