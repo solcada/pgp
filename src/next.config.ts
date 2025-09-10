60c3c4386ce1b5d1460af1268a196feb650ae83c
solcada <andrew.dotchin@solcada.com>
2025-09-11 09:47:47 +1200

    adding

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+
 // src/next.config.js
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'out',
+    basePath: repoName ? `/${repoName}` : "",
+    assetPrefix: repoName ? `/${repoName}/` : "",
+    images: { unoptimized: true },
+    trailingSlash: true,    
 };
 
 export default nextConfig;
