9059ce9ff27a6c07252e7ceeb9ae90f8af325f90
solcada <andrew.dotchin@solcada.com>
2025-09-11 09:27:22 +1200

    out

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 // src/next.config.js
 const nextConfig: NextConfig = {
     output: 'export',
-    distDir: './out',
+    distDir: 'out',
 };
 
 export default nextConfig;
